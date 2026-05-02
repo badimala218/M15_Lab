7f882b9c8299a3e77de1423e7b842230a670e6fd
badimala218 <badimala@ucsd.edu>
2026-05-02 02:03:27 -0700

    Added Webpack config and package.json script

--- public/employees.js
+++ public/employees.js
@@ -1,54 +1,41 @@
-class EmployeeFilter extends React.Component {
-  render() {
-    return /*#__PURE__*/React.createElement("div", null, "This is a placeholder for employee filter.");
+"use strict";
+
+function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
+var _EmployeeAdd = _interopRequireDefault(require("./EmployeeAdd.jsx"));
+function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
+function _classCallCheck(a, n) { if (!(a instanceof n)) throw new TypeError("Cannot call a class as a function"); }
+function _defineProperties(e, r) { for (var t = 0; t < r.length; t++) { var o = r[t]; o.enumerable = o.enumerable || !1, o.configurable = !0, "value" in o && (o.writable = !0), Object.defineProperty(e, _toPropertyKey(o.key), o); } }
+function _createClass(e, r, t) { return r && _defineProperties(e.prototype, r), t && _defineProperties(e, t), Object.defineProperty(e, "prototype", { writable: !1 }), e; }
+function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : i + ""; }
+function _toPrimitive(t, r) { if ("object" != _typeof(t) || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != _typeof(i)) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
+function _callSuper(t, o, e) { return o = _getPrototypeOf(o), _possibleConstructorReturn(t, _isNativeReflectConstruct() ? Reflect.construct(o, e || [], _getPrototypeOf(t).constructor) : o.apply(t, e)); }
+function _possibleConstructorReturn(t, e) { if (e && ("object" == _typeof(e) || "function" == typeof e)) return e; if (void 0 !== e) throw new TypeError("Derived constructors may only return object or undefined"); return _assertThisInitialized(t); }
+function _assertThisInitialized(e) { if (void 0 === e) throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); return e; }
+function _isNativeReflectConstruct() { try { var t = !Boolean.prototype.valueOf.call(Reflect.construct(Boolean, [], function () {})); } catch (t) {} return (_isNativeReflectConstruct = function _isNativeReflectConstruct() { return !!t; })(); }
+function _getPrototypeOf(t) { return _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf.bind() : function (t) { return t.__proto__ || Object.getPrototypeOf(t); }, _getPrototypeOf(t); }
+function _inherits(t, e) { if ("function" != typeof e && null !== e) throw new TypeError("Super expression must either be null or a function"); t.prototype = Object.create(e && e.prototype, { constructor: { value: t, writable: !0, configurable: !0 } }), Object.defineProperty(t, "prototype", { writable: !1 }), e && _setPrototypeOf(t, e); }
+function _setPrototypeOf(t, e) { return _setPrototypeOf = Object.setPrototypeOf ? Object.setPrototypeOf.bind() : function (t, e) { return t.__proto__ = e, t; }, _setPrototypeOf(t, e); }
+var EmployeeFilter = /*#__PURE__*/function (_React$Component) {
+  function EmployeeFilter() {
+    _classCallCheck(this, EmployeeFilter);
+    return _callSuper(this, EmployeeFilter, arguments);
   }
-}
-class EmployeeAdd extends React.Component {
-  constructor() {
-    super();
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-  handleSubmit(e) {
-    e.preventDefault();
-    const form = document.forms.employeeAdd;
-    const employee = {
-      name: form.name.value,
-      extension: form.ext.value,
-      email: form.email.value,
-      title: form.title.value
-    };
-    this.props.createEmployee(employee);
-    form.name.value = '';
-    form.ext.value = '';
-    form.email.value = '';
-    form.title.value = '';
-  }
-  render() {
-    return /*#__PURE__*/React.createElement("form", {
-      name: "employeeAdd",
-      onSubmit: this.handleSubmit
-    }, "Name: ", /*#__PURE__*/React.createElement("input", {
-      type: "text",
-      name: "name"
-    }), /*#__PURE__*/React.createElement("br", null), "Extension: ", /*#__PURE__*/React.createElement("input", {
-      type: "text",
-      name: "ext",
-      maxLength: 4
-    }), /*#__PURE__*/React.createElement("br", null), "Email: ", /*#__PURE__*/React.createElement("input", {
-      type: "text",
-      name: "email"
-    }), /*#__PURE__*/React.createElement("br", null), "Title ", /*#__PURE__*/React.createElement("input", {
-      type: "text",
-      name: "title"
-    }), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("button", null, "Add"));
-  }
-}
+  _inherits(EmployeeFilter, _React$Component);
+  return _createClass(EmployeeFilter, [{
+    key: "render",
+    value: function render() {
+      return /*#__PURE__*/React.createElement("div", null, "This is a placeholder for employee filter.");
+    }
+  }]);
+}(React.Component);
 function EmployeeTable(props) {
-  const employeeRows = props.employees.map(employee => /*#__PURE__*/React.createElement(EmployeeRow, {
-    key: employee._id,
-    employee: employee,
-    deleteEmployee: props.deleteEmployee
-  }));
+  var employeeRows = props.employees.map(function (employee) {
+    return /*#__PURE__*/React.createElement(EmployeeRow, {
+      key: employee._id,
+      employee: employee,
+      deleteEmployee: props.deleteEmployee
+    });
+  });
   return /*#__PURE__*/React.createElement("table", {
     className: "bordered-table"
   }, /*#__PURE__*/React.createElement("thead", null, /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("th", null, "Name"), /*#__PURE__*/React.createElement("th", null, "Ext"), /*#__PURE__*/React.createElement("th", null, "Email"), /*#__PURE__*/React.createElement("th", null, "Title"), /*#__PURE__*/React.createElement("th", null, "Date"), /*#__PURE__*/React.createElement("th", null, "Currently Employed?"), /*#__PURE__*/React.createElement("th", null))), /*#__PURE__*/React.createElement("tbody", null, employeeRows));
@@ -61,68 +48,90 @@ function EmployeeRow(props) {
     onClick: onDeleteClick
   }, "DELETE")));
 }
-class EmployeeList extends React.Component {
-  constructor() {
-    super();
-    this.state = {
+var EmployeeList = /*#__PURE__*/function (_React$Component2) {
+  function EmployeeList() {
+    var _this;
+    _classCallCheck(this, EmployeeList);
+    _this = _callSuper(this, EmployeeList);
+    _this.state = {
       employees: []
     };
-    this.createEmployee = this.createEmployee.bind(this);
-    this.deleteEmployee = this.deleteEmployee.bind(this);
+    _this.createEmployee = _this.createEmployee.bind(_this);
+    _this.deleteEmployee = _this.deleteEmployee.bind(_this);
+    return _this;
   }
-  componentDidMount() {
-    this.loadData();
-  }
-  loadData() {
-    fetch('/api/employees').then(response => response.json()).then(data => {
-      console.log('Total count of employees:', data.count);
-      data.employees.forEach(employee => {
-        employee.dateHired = new Date(employee.dateHired);
+  _inherits(EmployeeList, _React$Component2);
+  return _createClass(EmployeeList, [{
+    key: "componentDidMount",
+    value: function componentDidMount() {
+      this.loadData();
+    }
+  }, {
+    key: "loadData",
+    value: function loadData() {
+      var _this2 = this;
+      fetch('/api/employees').then(function (response) {
+        return response.json();
+      }).then(function (data) {
+        console.log('Total count of employees:', data.count);
+        data.employees.forEach(function (employee) {
+          employee.dateHired = new Date(employee.dateHired);
+        });
+        _this2.setState({
+          employees: data.employees
+        });
+      })["catch"](function (err) {
+        console.log(err);
       });
-      this.setState({
-        employees: data.employees
+    }
+  }, {
+    key: "createEmployee",
+    value: function createEmployee(employee) {
+      var _this3 = this;
+      fetch('/api/employees', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(employee)
+      }).then(function (response) {
+        return response.json();
+      }).then(function (newEmployee) {
+        newEmployee.employee.dateHired = new Date(newEmployee.employee.dateHired);
+        var newEmployees = _this3.state.employees.concat(newEmployee.employee);
+        _this3.setState({
+          employees: newEmployees
+        });
+        console.log('Total count of employees:', newEmployees.length);
+      })["catch"](function (err) {
+        console.log(err);
       });
-    }).catch(err => {
-      console.log(err);
-    });
-  }
-  createEmployee(employee) {
-    fetch('/api/employees', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(employee)
-    }).then(response => response.json()).then(newEmployee => {
-      newEmployee.employee.dateHired = new Date(newEmployee.employee.dateHired);
-      const newEmployees = this.state.employees.concat(newEmployee.employee);
-      this.setState({
-        employees: newEmployees
+    }
+  }, {
+    key: "deleteEmployee",
+    value: function deleteEmployee(id) {
+      var _this4 = this;
+      fetch("/api/employees/".concat(id), {
+        method: 'DELETE'
+      }).then(function (response) {
+        if (!response.ok) {
+          console.log('Failed to delete employee.');
+        } else {
+          _this4.loadData();
+        }
       });
-      console.log('Total count of employees:', newEmployees.length);
-    }).catch(err => {
-      console.log(err);
-    });
-  }
-  deleteEmployee(id) {
-    fetch(`/api/employees/${id}`, {
-      method: 'DELETE'
-    }).then(response => {
-      if (!response.ok) {
-        console.log('Failed to delete employee.');
-      } else {
-        this.loadData();
-      }
-    });
-  }
-  render() {
-    return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "Employee Management App"), /*#__PURE__*/React.createElement(EmployeeFilter, null), /*#__PURE__*/React.createElement("hr", null), /*#__PURE__*/React.createElement(EmployeeTable, {
-      employees: this.state.employees,
-      deleteEmployee: this.deleteEmployee
-    }), /*#__PURE__*/React.createElement("hr", null), /*#__PURE__*/React.createElement(EmployeeAdd, {
-      createEmployee: this.createEmployee
-    }));
-  }
-}
-const contentNode = document.getElementById('content');
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "Employee Management App"), /*#__PURE__*/React.createElement(EmployeeFilter, null), /*#__PURE__*/React.createElement("hr", null), /*#__PURE__*/React.createElement(EmployeeTable, {
+        employees: this.state.employees,
+        deleteEmployee: this.deleteEmployee
+      }), /*#__PURE__*/React.createElement("hr", null), /*#__PURE__*/React.createElement(_EmployeeAdd["default"], {
+        createEmployee: this.createEmployee
+      }));
+    }
+  }]);
+}(React.Component);
+var contentNode = document.getElementById('content');
 ReactDOM.render(/*#__PURE__*/React.createElement(EmployeeList, null), contentNode);
--- src/employees.jsx
+++ src/employees.jsx
@@ -1,40 +1,8 @@
+import EmployeeAdd from './EmployeeAdd.jsx'
 
 class EmployeeFilter extends React.Component {
     render() {
         return (<div>This is a placeholder for employee filter.</div>)
-    }
-}
-
-class EmployeeAdd extends React.Component {
-    constructor() {
-        super()
-        this.handleSubmit = this.handleSubmit.bind(this)
-    }
-    handleSubmit(e) {
-        e.preventDefault()
-        const form = document.forms.employeeAdd
-        const employee = {
-            name: form.name.value,
-            extension: form.ext.value,
-            email: form.email.value,
-            title: form.title.value
-        }
-        this.props.createEmployee(employee)
-        form.name.value = ''
-        form.ext.value = ''
-        form.email.value = ''
-        form.title.value = ''
-    }
-    render() {
-        return (
-            <form name="employeeAdd" onSubmit={this.handleSubmit}>
-                Name: <input type="text" name="name"/><br/>
-                Extension: <input type="text" name="ext" maxLength={4}/><br/>
-                Email: <input type="text" name="email"/><br/>
-                Title <input type="text" name="title"/><br/>
-                <button>Add</button>
-            </form>
-        )
     }
 }
 
